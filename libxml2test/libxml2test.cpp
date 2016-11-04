f985e6e7f591424523e0cd6d981db5cd0d1cc694
Tony Joblin <tony.joblin@silverrailtech.com>
2016-11-04 16:07:22 +1000

    Initial code following examples in docs
    Just reads xml file and prints element names.

--- libxml2test/libxml2test.cpp
+++ libxml2test/libxml2test.cpp
@@ -3,9 +3,56 @@
 
 #include "stdafx.h"
 
+#include <string>
+#include <iostream>
+
+#include <libxml/xmlreader.h>
+
+using namespace std;
+
+void DisplayLocalName(xmlTextReaderPtr reader) {
+    auto name = xmlTextReaderLocalName(reader);
+    cout << name << endl;
+    xmlFree(name);
+}
+
+void processNode(xmlTextReaderPtr reader) {
+    const int nodeType = xmlTextReaderNodeType(reader);
+    switch (nodeType) {
+    case XML_READER_TYPE_ELEMENT:
+        DisplayLocalName(reader);
+        break;
+    };
+}
+
+void streamFile(const string& filename) {
+
+    xmlTextReaderPtr reader;
+    int ret;
+
+    reader = xmlNewTextReaderFilename(filename.c_str());
+    if (reader != NULL) {
+        ret = xmlTextReaderRead(reader);
+        while (ret == 1) {
+            processNode(reader);
+            ret = xmlTextReaderRead(reader);
+        }
+        xmlFreeTextReader(reader);
+        if (ret != 0) {
+            cerr << "Failed to parse : " << filename << endl;
+        }
+    }
+    else {
+        cerr << "Unable to open " << filename << endl;
+    }
+}
 
 int main()
 {
+    string fileName = R"(D:\Archives\20160530020814_v7.xml)";
+
+    streamFile(fileName);
+
     return 0;
 }
 
